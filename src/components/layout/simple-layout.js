07d06700c713c2bee688095468a459540193579b
Isitha Subasinghe <isitha@MacBookProLocal>
2020-05-21 11:32:11 +1000

    fix: changed color for post page nav items

--- src/components/layout/simple-layout.js
+++ src/components/layout/simple-layout.js
@@ -35,14 +35,14 @@ const BackIcon = styled(FontAwesomeIcon)`
   height: 2rem;
   width: 1.25rem !important;
   path {
-    fill: ${theme.fill};
+    fill: ${theme.blogText};
   }
 `;
 
 const P = styled.p`
   margin-left: 1rem;
   font-size: 1.25rem;
-  color: ${theme.fill};
+  color: ${theme.blogText};
 `;
 const SimpleLayout = ({ children }) => {
   return (
